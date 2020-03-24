0f64aa71674eb90663af72267422a3bcd4424622
Javante Nelson <javantenelson@gmail.com>
2020-03-24 14:48:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,6 @@ def speak_to_grandma(string)
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else return "HUH?! SPEAK UP, SONNY!"
   end
 end
